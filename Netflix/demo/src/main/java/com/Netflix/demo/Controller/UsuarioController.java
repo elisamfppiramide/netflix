54d9f4a90d2b38c769ad4549f7b239ccf93fab77
elisamaurin <elisabelmseta@gmail.com>
2025-11-20 12:35:30 +0100

    falla el inicio

--- Netflix/demo/src/main/java/com/Netflix/demo/Controller/UsuarioController.java
+++ Netflix/demo/src/main/java/com/Netflix/demo/Controller/UsuarioController.java
@@ -5,12 +5,25 @@ import com.Netflix.demo.dao.DAOFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class UsuarioController {
+
+    @GetMapping("/inicio")
+    public String inicio() {
+        return "inicio";
+    }
+
+    @PostMapping("/inicio")
+    public String procesarInicio(@RequestParam String nombreUsuario) {
+        return "redirect:/listaPeliculas?nombreUsuario=" + nombreUsuario;
+    }
 
     @GetMapping("/listaUsuarios")
     public String listaUsuarios(Model model){
@@ -24,6 +37,13 @@ public class UsuarioController {
     public String addUsuario(Model model){
         model.addAttribute("usuario", new Usuario());
         return "addUsuario";
+    }
+
+    @PostMapping("/guardarUsuario")
+    public String guardarUsuario(@ModelAttribute("usuario") Usuario usuario) {
+        DAOFactory daoFactory = DAOFactory.getInstance();
+        daoFactory.getDaoUsuarios().insertarUsuario(usuario);
+        return "redirect:/listaPeliculas?nombreUsuario=" + usuario.getNombreUsuario();
     }
 
 
